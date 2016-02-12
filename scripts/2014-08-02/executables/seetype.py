4704f11b36edb2177b900880b93d413f5d74c6de
manuelmarcano22 <manuelmarcano22@yahoo.com>
2016-02-12 14:53:46 +0100

    Get RA,DEC and Lamp in seetype

--- scripts/2014-08-02/executables/seetype.py
+++ scripts/2014-08-02/executables/seetype.py
@@ -4,6 +4,7 @@
 #Need to be called thought bash script "createmastersof" to get the enviroment variables. 
 
 from astropy.io import fits
+import numpy as np
 import os
 import glob
 
@@ -13,6 +14,19 @@ os.chdir(direc)
 
 files = glob.glob('*.fz')
 dictype = {'BIAS': 'BIAS','DARK': 'DARK','FLAT,LAMP':'FLAT','LAMP,FLAT': 'FLAT','FLAT,SKY':'SKYFLAT','SKY,FLAT': 'SKYFLAT','WAVE':'ARC','WAVE,MASK':'MASK','MASK,WAVE':'MASK','STD':'STD','ASTROMETRY':'ASTROMETRY','SKY':'SKY','OBJECT':'OBJECT','FLAT,LAMP,ILLUM':'ILLUM'}
+
+
+def getlamp(temp):
+    templist=[0,0,0,0,0,0]
+    templist[0] = temp[0].header[559]
+    templist[1] = temp[0].header[562]
+    templist[2] = temp[0].header[565]
+    templist[3] = temp[0].header[568]
+    templist[4] = temp[0].header[571]
+    templist[5] = temp[0].header[574]
+    return np.where(templist)[0][0]+1
+
+
 mastersof = open('mastersof.txt','w')
 
 for f in files:
@@ -20,8 +34,18 @@ for f in files:
     temppath = os.path.realpath(f)
     typetemp = temp[0].header['*DPR TYPE*'][0]
     mastersof.write(temppath   +' '+  dictype[typetemp] + ' # ' + temp[0].header["*INS MODE"][0] 
-            +' '+ temp[0].header['*OBS NAME'][0]  +' '+ temp[0].header['*DATE-OBS*'][0] +' '+  str(temp[0].header['*TEMP11 VAL'][0]) + ' '+ temp[0].header['*READ CURNAME*'][0]   + '\n')
-    print temp[0].header['*DPR TYPE*'][0] , temp[0].header['*INS MODE'][0], temp[0].header['*OBS NAME'][0], temp[0].header['*DATE-OBS*'][0], temp[0].header['*TEMP11 VAL'][0],temp[0].header['*READ CURNAME*'][0]  
+            +' '+ temp[0].header['*OBS NAME'][0]  +' '+ temp[0].header['*DATE-OBS*'][0] +' '
+            +  str(temp[0].header['*TEMP11 VAL'][0]) + ' '+ temp[0].header['*READ CURNAME*'][0]   
+            +  (' lamp: #'+str(getlamp(temp)) if typetemp == 'ARC' else '')  
+            + (' RA:'+ str(temp[0].header['RA']) if typetemp=='ASTROMETRY' or typetemp=='STD' or typetemp=='OBJECT' else '')   
+            + (' DEC:'+ str(temp[0].header['DEC']) if typetemp=='ASTROMETRY' or typetemp=='STD' or typetemp=='OBJECT'  else '' )    
+            + '\n')
+    print temp[0].header['*DPR TYPE*'][0] , temp[0].header['*INS MODE'][0] \
+        , temp[0].header['*OBS NAME'][0], temp[0].header['*DATE-OBS*'][0], temp[0].header['*TEMP11 VAL'][0],temp[0].header['*READ CURNAME*'][0] \
+        , (' lamp: #'+str(getlamp(temp)) if typetemp == 'WAVE' else '')  , (' RA:'+ str(temp[0].header['RA']) if typetemp=='ASTROMETRY' or typetemp=='STD' else '') \
+        , (' DEC:'+ str(temp[0].header['DEC']) if typetemp=='ASTROMETRY' or typetemp=='STD'  else '' )    
+
+
     temp.close()
 mastersof.close()
 os.system('sort -k5 -k2 -k3  -k6 mastersof.txt > temp.txt')
