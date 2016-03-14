792c24015dc208c26ee98157344b4615172a749e
manuelmarcano22 <manuelmarcano22@yahoo.com>
2016-03-14 17:51:13 +0100

    Data for all observation days

---
+++ scripts/2014-07-30/executables/arc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python2
+""" Program to create a list of the ARC to be use for the wavelenght calibration and see their lamps. """
+
+from astropy.io import fits
+import os
+import numpy as np
+
+direc = os.environ['ESOREX_OUTPUT_DIR']
+os.chdir(direc)
+
+filearc = open('filearc.txt', 'w')
+
+
+templist=[0,0,0,0,0,0]
+dict1 = {'T':'True', 'F':'False'}
+with open('arctodos.txt') as fp:
+	for line in fp:
+		temp1 = line.split()
+		temp = fits.open(temp1[0])
+		templist[0] = temp[0].header[559]
+		templist[1] = temp[0].header[562]
+		templist[2] = temp[0].header[565]
+		templist[3] = temp[0].header[568]
+		templist[4] = temp[0].header[571]
+		templist[5] = temp[0].header[574]
+		print np.where(templist)[0][0]+1
+		filearc.write(temp1[0]+' ' +'ARC'+' #'+str(np.where(templist)[0][0]+1)+'\n')
+		temp.close()
+filearc.close()
+os.system('rm arctodos.txt')			
---
+++ scripts/2014-07-30/executables/seetype.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python2
+# It creates a file with the path and the Frame tag depending on its TYPE as stated in Table 
+# 6.1 in the MUSE PIPELINE MANUAL
+#Need to be called thought bash script "createmastersof" to get the enviroment variables. 
+
+from astropy.io import fits
+import numpy as np
+import os
+import glob
+
+#Change to where the data is:
+direc = os.environ['ESOREX_OUTPUT_DIR']
+os.chdir(direc)
+
+files = glob.glob('*.fz')
+dictype = {'BIAS': 'BIAS','DARK': 'DARK','FLAT,LAMP':'FLAT','LAMP,FLAT': 'FLAT','FLAT,SKY':'SKYFLAT','SKY,FLAT': 'SKYFLAT','WAVE':'ARC','WAVE,MASK':'MASK','MASK,WAVE':'MASK','STD':'STD','ASTROMETRY':'ASTROMETRY','SKY':'SKY','OBJECT':'OBJECT','FLAT,LAMP,ILLUM':'ILLUM'}
+
+
+def getlamp(temp):
+    templist=[0,0,0,0,0,0]
+    templist[0] = temp[0].header[559]
+    templist[1] = temp[0].header[562]
+    templist[2] = temp[0].header[565]
+    templist[3] = temp[0].header[568]
+    templist[4] = temp[0].header[571]
+    templist[5] = temp[0].header[574]
+    return np.where(templist)[0][0]+1
+
+
+mastersof = open('mastersof.txt','w')
+
+for f in files:
+    temp = fits.open(str(f))
+    temppath = os.path.realpath(f)
+    typetemp = temp[0].header['*DPR TYPE*'][0]
+    mastersof.write(temppath   +' '+  dictype[typetemp] + ' # ' + temp[0].header["*INS MODE"][0] 
+            +' '+ temp[0].header['*OBS NAME'][0]  +' '+ temp[0].header['*DATE-OBS*'][0] +' '
+            +  str(temp[0].header['*TEMP11 VAL'][0]) + ' '+ temp[0].header['*READ CURNAME*'][0]   
+            +  (' lamp: #'+str(getlamp(temp)) if typetemp == 'ARC' else '')  
+#            + (' RA:'+ str(temp[0].header['RA']) if typetemp=='ASTROMETRY' or typetemp=='STD' or typetemp=='OBJECT' else '')   
+#            + (' DEC:'+ str(temp[0].header['DEC']) if typetemp=='ASTROMETRY' or typetemp=='STD' or typetemp=='OBJECT'  else '' )    
++ (temp[0].header.comments['RA'][5:20] if typetemp=='ASTROMETRY' or typetemp=='STD' or typetemp=='OBJECT' else '')   
++ (temp[0].header.comments['DEC'][6:29] if typetemp=='ASTROMETRY' or typetemp=='STD' or typetemp=='OBJECT'  else '' )    
+            + '\n')
+    print temp[0].header['*DPR TYPE*'][0] , temp[0].header['*INS MODE'][0] \
+        , temp[0].header['*OBS NAME'][0], temp[0].header['*DATE-OBS*'][0], temp[0].header['*TEMP11 VAL'][0],temp[0].header['*READ CURNAME*'][0] \
+        , (' lamp: #'+str(getlamp(temp)) if typetemp == 'WAVE' else '') , (' RA:'+ str(temp[0].header['RA']) if typetemp=='ASTROMETRY' or typetemp=='OBJECT' or typetemp=='STD' else '') \
+        , (' DEC:'+ str(temp[0].header['DEC']) if typetemp=='ASTROMETRY' or typetemp=='STD' or typetemp=='OBJECT'  else '' )    
+
+
+    temp.close()
+mastersof.close()
+os.system('sort -k5 -k2 -k3  -k6 mastersof.txt > temp.txt')
+os.system('cp temp.txt mastersof.txt')
+os.system('rm temp.txt')
